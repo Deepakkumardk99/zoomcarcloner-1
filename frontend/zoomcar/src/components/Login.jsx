801f69e371fcfc22a4a4bd31e3918e1ae6d216c1
Deepakkumardk99 <deepakkumarkingmaker@gmail.com>
2024-09-02 18:50:08 +0530

    updated

--- frontend/zoomcar/src/components/Login.jsx
+++ frontend/zoomcar/src/components/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/auth/login`, {
+      const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}auth/login`, {
         email,
         password,
       });
